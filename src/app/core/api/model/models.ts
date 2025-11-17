fa08bb7bd6c3289ef16a1970e271dd989104420f
tiagosantosp <tiago_s.p@hotmail.com>
2025-11-17 16:15:22 -0300

    Criando as models com as classes utilizadas no backend

---
+++ src/app/core/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './arquivoDetalhadoDto';
+export * from './arquivoDto';
+export * from './estatisticasDto';
+export * from './problemDetails';
+export * from './transacaoArquivoDto';
+export * from './uploadResultDto';
+export * from './validationProblemDetails';
