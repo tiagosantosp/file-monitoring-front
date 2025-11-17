41691d7404d7c676a580517ebe69ca6d2ddd63d5
tiagosantosp <tiago_s.p@hotmail.com>
2025-11-17 16:14:14 -0300

    adicionando as services para requisicoes ao banco de dados

---
+++ src/app/core/api/api/api.ts
@@ -0,0 +1,5 @@
+export * from './arquivos.service';
+import { ArquivosService } from './arquivos.service';
+export * from './dashboard.service';
+import { DashboardService } from './dashboard.service';
+export const APIS = [ArquivosService, DashboardService];
