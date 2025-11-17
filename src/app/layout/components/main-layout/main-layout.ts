0038eb26c3adcdf771261ecc7cf5a0a33d91a458
tiagosantosp <tiago_s.p@hotmail.com>
2025-11-17 16:21:25 -0300

    adicionando componente de layout para aplicacao

---
+++ src/app/layout/components/main-layout/main-layout.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss',
+})
+export class MainLayoutComponent {
+  title = 'File Monitoring';
+}
